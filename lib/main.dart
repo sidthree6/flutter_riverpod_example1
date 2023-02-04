775432ab512f0455bcd74e8c5e354ea725b5d022
sidthree6 <psiddhu001@gmail.com>
2023-02-03 21:10:05 -0500

    Example 1 finished

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(
-    const App(),
-  );
+  runApp(const ProviderScope(
+    child: App(),
+  ));
 }
 
 class App extends StatelessWidget {
